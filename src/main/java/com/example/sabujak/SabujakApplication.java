dcfb4094e048680a5d0a07837e3f343d1d212f55
yurim0628 <dbfla0303@naver.com>
2024-04-28 22:39:48 +0900

    Create Project

---
+++ src/main/java/com/example/sabujak/SabujakApplication.java
@@ -0,0 +1,13 @@
+package com.example.sabujak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabujakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabujakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sabujak/SabujakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sabujak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabujakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
