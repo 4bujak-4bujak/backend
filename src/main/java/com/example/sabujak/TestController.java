58147d53a5e05bacd2eadef9e3ac79c962ff4640
yurim0628 <dbfla0303@naver.com>
2024-04-29 00:34:19 +0900

    feat: 레디스 테스트 컨트롤러 작성

--- src/main/java/com/example/sabujak/TestController.java
+++ src/main/java/com/example/sabujak/TestController.java
@@ -1,5 +1,8 @@
 package com.example.sabujak;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @GetMapping
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("test");
+    }
+
+    @GetMapping("/redis-test")
+    public ResponseEntity<String> redis() {
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+        operations.set("test", "test");
+        return ResponseEntity.ok((String)operations.get("test"));
     }
 }
