0c119e68d2b2599f46667389bf9a8ce2c468d5d8
zoomin3022 <zoomin3022@gmail.com>
2024-05-04 18:01:42 +0900

    refactor: #5 CustomException  class -> abstract class

--- src/main/java/com/example/sabujak/common/exception/CustomException.java
+++ src/main/java/com/example/sabujak/common/exception/CustomException.java
@@ -3,12 +3,7 @@ package com.example.sabujak.common.exception;
 import lombok.Getter;
 
 @Getter
-public class CustomException extends RuntimeException {
+public abstract class CustomException extends RuntimeException {
 
-    private final ErrorCode errorCode;
-
-    public CustomException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
+    public abstract ErrorCode getErrorCode();
 }
