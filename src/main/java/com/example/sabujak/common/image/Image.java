e23089c5c7774bc376f59421e603d3fd35b4e0fd
KimSangwoo1259 <rlatkddn2902@gmail.com>
2024-05-17 17:40:10 +0900

    feat: #21 image entity 생성

---
+++ src/main/java/com/example/sabujak/common/image/Image.java
@@ -0,0 +1,36 @@
+package com.example.sabujak.common.image;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@Entity
+@Table(name = "image")
+@DiscriminatorColumn
+public abstract class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long imageId;
+
+    @NotNull
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    public Image(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
