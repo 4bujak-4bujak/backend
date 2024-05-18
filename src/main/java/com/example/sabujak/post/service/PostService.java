260e1d4ee9a430fd1195eeb645e843dcf071e7df
yurim0628 <dbfla0303@naver.com>
2024-05-19 00:31:28 +0900

    feat: #19 PostService 클래스 savePostLike 메서드 작성

--- src/main/java/com/example/sabujak/post/service/PostService.java
+++ src/main/java/com/example/sabujak/post/service/PostService.java
@@ -2,9 +2,14 @@ package com.example.sabujak.post.service;
 
 import com.example.sabujak.member.entity.Member;
 import com.example.sabujak.member.repository.MemberRepository;
+import com.example.sabujak.post.dto.PostLikeSaveRequest;
 import com.example.sabujak.post.dto.PostSaveRequest;
 import com.example.sabujak.post.dto.PostSaveResponse;
 import com.example.sabujak.post.entity.Post;
+import com.example.sabujak.post.entity.PostLike;
+import com.example.sabujak.post.entity.PostLikeId;
+import com.example.sabujak.post.exception.PostException;
+import com.example.sabujak.post.repository.PostLikeRepository;
 import com.example.sabujak.post.repository.PostRepository;
 import com.example.sabujak.security.exception.AuthException;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.example.sabujak.post.exception.PostErrorCode.POST_NOT_FOUND;
 import static com.example.sabujak.security.exception.AuthErrorCode.ACCOUNT_NOT_EXISTS;
 
 @Slf4j
@@ -21,6 +27,7 @@ public class PostService {
 
     private final MemberRepository memberRepository;
     private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
 
     @Transactional
     public PostSaveResponse savePost(PostSaveRequest postSaveRequest, String email) {
@@ -34,13 +41,38 @@ public class PostService {
                 .build();
         post.setMember(member);
 
-        log.info("Saving a new post for member with email: {}", email);
+        log.info("Saving post for member with email: [{}]", email);
 
         return PostSaveResponse.of(postRepository.save(post));
+    }
+
+    @Transactional
+    public void savePostLike(PostLikeSaveRequest postLikeSaveRequest, String email) {
+        Member member = findMember(email);
+
+        Post post = findPost(postLikeSaveRequest.postId());
+        post.increaseLikeCount();
+
+        PostLike postLike = PostLike.builder()
+                .id(
+                        PostLikeId.builder()
+                                .postId(post.getId())
+                                .memberId(member.getMemberId())
+                                .build()
+                )
+                .build();
+        postLikeRepository.save(postLike);
+
+        log.info("Saved post like for member with email: [{}] post ID: [{}]", email, post.getId());
     }
 
     public Member findMember(String email) {
         return memberRepository.findByMemberEmail(email)
                 .orElseThrow(() -> new AuthException(ACCOUNT_NOT_EXISTS));
+    }
+
+    public Post findPost(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new PostException(POST_NOT_FOUND));
     }
 }
