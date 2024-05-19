23f59d4497dda235a66c2cce107aadbd4d639723
yurim0628 <dbfla0303@naver.com>
2024-05-19 16:56:45 +0900

    feat: #19 CommentController - saveComment 메서드 작성

---
+++ src/main/java/com/example/sabujak/comment/controller/CommentController.java
@@ -0,0 +1,30 @@
+package com.example.sabujak.comment.controller;
+
+import com.example.sabujak.comment.dto.SaveCommentRequest;
+import com.example.sabujak.comment.service.CommentService;
+import com.example.sabujak.common.response.Response;
+import com.example.sabujak.security.dto.request.AuthRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts/{postId}/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Response<Void>> saveComment(
+            @PathVariable Long postId,
+            @RequestBody @Validated SaveCommentRequest saveCommentRequest,
+            @AuthenticationPrincipal AuthRequestDto.Access access
+    ) {
+        String email = access.getEmail();
+        commentService.saveComment(postId, saveCommentRequest, email);
+        return ResponseEntity.ok(Response.success());
+    }
+}
