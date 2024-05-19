21d7bf87c0700241e185d8ecf8b10d4d8fa55436
yurim0628 <dbfla0303@naver.com>
2024-05-19 18:25:58 +0900

    feat: #19 findWithMemberAndPostByPostIdAndCommentId 메서드 작성

--- src/main/java/com/example/sabujak/comment/repository/CommentRepository.java
+++ src/main/java/com/example/sabujak/comment/repository/CommentRepository.java
@@ -2,6 +2,21 @@ package com.example.sabujak.comment.repository;
 
 import com.example.sabujak.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query(
+            value = "select c " +
+                    "from Comment c " +
+                    "join fetch c.member m " +
+                    "join fetch c.post p " +
+                    "where c.post.id = :postId and c.id = :commentId")
+    Optional<Comment> findWithMemberAndPostByPostIdAndCommentId(
+            @Param("postId") Long postId,
+            @Param("commentId") Long commentId
+    );
 }
