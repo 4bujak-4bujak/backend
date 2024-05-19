172765051b59874347436ddb59674b7dac626e05
yurim0628 <dbfla0303@naver.com>
2024-05-19 12:47:35 +0900

    feat: #19 findWithMemberById 메서드 작성

--- src/main/java/com/example/sabujak/post/repository/PostRepository.java
+++ src/main/java/com/example/sabujak/post/repository/PostRepository.java
@@ -2,6 +2,18 @@ package com.example.sabujak.post.repository;
 
 import com.example.sabujak.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query(
+            value = "select p " +
+                    "from Post p " +
+                    "join fetch p.member " +
+                    "where p.id = :postId"
+    )
+    Optional<Post> findWithMemberById(@Param("postId") Long postId);
 }
