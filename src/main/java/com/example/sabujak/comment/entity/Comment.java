0226c74cb2dd3b8781747b9b7cd9b00474efe559
yurim0628 <dbfla0303@naver.com>
2024-05-19 16:59:02 +0900

    feat: #19 Comment Entity 생성

---
+++ src/main/java/com/example/sabujak/comment/entity/Comment.java
@@ -0,0 +1,47 @@
+package com.example.sabujak.comment.entity;
+
+import com.example.sabujak.common.entity.BaseEntity;
+import com.example.sabujak.member.entity.Member;
+import com.example.sabujak.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    private Comment(String content) {
+        this.content = content;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
