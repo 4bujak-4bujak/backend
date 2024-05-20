f6e73b742aa82bc4b6b687368a0b089865326dde
zoomin3022 <zoomin3022@gmail.com>
2024-05-20 16:56:58 +0900

    bug: #23 Image CreatedAt 에 EntityListners 추가

--- src/main/java/com/example/sabujak/common/image/Image.java
+++ src/main/java/com/example/sabujak/common/image/Image.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "image")
 @DiscriminatorColumn
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Image {
 
     @Id
