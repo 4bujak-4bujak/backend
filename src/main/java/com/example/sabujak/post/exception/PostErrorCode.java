4f8ab57a4fae2c3921e21ec00b60c3af702b14c5
yurim0628 <dbfla0303@naver.com>
2024-05-21 05:26:10 +0900

    refactor: #25 Comment ErrorCode -> Post ErrorCode 이동

--- src/main/java/com/example/sabujak/post/exception/PostErrorCode.java
+++ src/main/java/com/example/sabujak/post/exception/PostErrorCode.java
@@ -13,7 +13,9 @@ public enum PostErrorCode implements ErrorCode {
 
     POST_NOT_FOUND(BAD_REQUEST, "3-001", "존재하지 않는 게시글 입니다."),
     POST_DELETE_DENIED(BAD_REQUEST, "3-002", "게시글 삭제 권한이 존재하지 않습니다."),
-    POST_LIKE_NOT_FOUND(BAD_REQUEST, "3-003", "존재하지 않는 관심 글 입니다.");
+    POST_LIKE_NOT_FOUND(BAD_REQUEST, "3-003", "존재하지 않는 관심 글 입니다."),
+    COMMENT_NOT_FOUND(BAD_REQUEST, "3-004", "존재하지 않는 댓글 입니다."),
+    COMMENT_DELETE_DENIED(BAD_REQUEST, "3-005", "댓글 삭제 권한이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String customCode;
