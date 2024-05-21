06acbf242720c37b0d6a953bd4a3fa6399b59cac
yurim0628 <dbfla0303@naver.com>
2024-05-21 18:31:47 +0900

    feat: #26 findAllWithMembersByCategoryAndId 메서드 작성

--- src/main/java/com/example/sabujak/post/repository/PostRepository.java
+++ src/main/java/com/example/sabujak/post/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.example.sabujak.post.repository;
 
+import com.example.sabujak.post.entity.Category;
 import com.example.sabujak.post.entity.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -16,4 +19,17 @@ public interface PostRepository extends JpaRepository<Post, Long> {
                     "where p.id = :postId"
     )
     Optional<Post> findWithMemberById(@Param("postId") Long postId);
+
+    @Query(
+            value = "select p " +
+                    "from Post p " +
+                    "join fetch p.member m " +
+                    "where (:category is null or p.category = :category) and " +
+                    "(:cursorId is null or p.id < :cursorId)"
+    )
+    List<Post> findAllWithMembersByCategoryAndId(
+            @Param("category") Category category,
+            @Param("cursorId") Long cursorId,
+            Pageable pageable
+    );
 }
