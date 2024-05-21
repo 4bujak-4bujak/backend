c3e48591d79f8079aed241a80f67202592250767
yurim0628 <dbfla0303@naver.com>
2024-05-21 18:35:37 +0900

    feat: #26 PaginationConstants 생성

---
+++ src/main/java/com/example/sabujak/post/constants/PaginationConstants.java
@@ -0,0 +1,8 @@
+package com.example.sabujak.post.constants;
+
+public class PaginationConstants {
+    public static final int DEFAULT_PAGE = 0;
+    public static final int DEFAULT_POST_PAGE_SIZE = 6;
+    public static final int ORIGIN_POST_PAGE_SIZE = 5;
+    public static final String DEFAULT_SORT_FIELD = "id";
+}
