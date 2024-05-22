9dd37cbe477d6a8abb42517a9579435f9a3138fa
yurim0628 <dbfla0303@naver.com>
2024-05-22 19:15:55 +0900

    feat: #26 findComments - findComments 메서드 작성

--- src/main/java/com/example/sabujak/post/service/CommentService.java
+++ src/main/java/com/example/sabujak/post/service/CommentService.java
@@ -1,18 +1,38 @@
 package com.example.sabujak.post.service;
 
+import com.example.sabujak.post.dto.CustomSlice;
 import com.example.sabujak.post.entity.Comment;
 import com.example.sabujak.post.repository.CommentRepository;
 import com.example.sabujak.post.exception.PostException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.example.sabujak.post.constants.PaginationConstants.ORIGIN_COMMENT_PAGE_SIZE;
 import static com.example.sabujak.post.exception.PostErrorCode.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
+
+    public CustomSlice<Comment> findComments(Long postId, Long cursorId, Pageable pageable) {
+        List<Comment> comments = commentRepository.findWithMemberByPostIdAndCursorId(postId, cursorId,pageable);
+        boolean hasNext = comments.size() > ORIGIN_COMMENT_PAGE_SIZE;
+        log.info("Get [{}] Comments. One More Retrieved: [{}]", comments.size(), hasNext);
+        if(hasNext) {
+            comments = comments.stream()
+                    .limit(ORIGIN_COMMENT_PAGE_SIZE)
+                    .collect(Collectors.toList());
+        }
+        return new CustomSlice<>(comments, hasNext);
+    }
 
     public Comment findCommentWithMemberAndPost(Long postId, Long commentId) {
         return commentRepository.findWithMemberAndPostByPostIdAndCommentId(postId, commentId)
