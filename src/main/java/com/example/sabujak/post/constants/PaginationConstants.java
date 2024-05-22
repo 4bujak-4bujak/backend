cf31d9cd1d89721757994506fdcf22866727eb11
yurim0628 <dbfla0303@naver.com>
2024-05-22 19:15:17 +0900

    feat: #26 페이지네이션 상수 추가

--- src/main/java/com/example/sabujak/post/constants/PaginationConstants.java
+++ src/main/java/com/example/sabujak/post/constants/PaginationConstants.java
@@ -4,5 +4,7 @@ public class PaginationConstants {
     public static final int DEFAULT_PAGE = 0;
     public static final int DEFAULT_POST_PAGE_SIZE = 6;
     public static final int ORIGIN_POST_PAGE_SIZE = 5;
+    public static final int DEFAULT_COMMENT_PAGE_SIZE = 11;
+    public static final int ORIGIN_COMMENT_PAGE_SIZE = 10;
     public static final String DEFAULT_SORT_FIELD = "id";
 }
