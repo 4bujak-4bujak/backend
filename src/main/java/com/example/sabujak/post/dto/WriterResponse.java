cf3bbfd6d53612ef7d14d67599671607c7f8c00f
yurim0628 <dbfla0303@naver.com>
2024-05-24 21:55:07 +0900

    feat: #38 사용자 이미지 응답 필드 추가

--- src/main/java/com/example/sabujak/post/dto/WriterResponse.java
+++ src/main/java/com/example/sabujak/post/dto/WriterResponse.java
@@ -4,11 +4,13 @@ import com.example.sabujak.member.entity.Job;
 import com.example.sabujak.member.entity.Member;
 
 public record WriterResponse(
+        String profile,
         Job job,
         String nickname
 ) {
     public static WriterResponse of(Member member){
         return new WriterResponse(
+                member.getImage().getImageUrl(),
                 member.getMemberJob(),
                 member.getMemberNickname()
         );
