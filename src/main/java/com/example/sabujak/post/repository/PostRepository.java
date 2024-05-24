acdb49d599d0b9d25282f1dc7addb437744710a7
yurim0628 <dbfla0303@naver.com>
2024-05-24 21:48:23 +0900

    feat: #38 findWithMemberAndImagesById 메서드 생성

--- src/main/java/com/example/sabujak/post/repository/PostRepository.java
+++ src/main/java/com/example/sabujak/post/repository/PostRepository.java
@@ -4,14 +4,11 @@ import com.example.sabujak.post.entity.Category;
 import com.example.sabujak.post.entity.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
-
-import static jakarta.persistence.LockModeType.PESSIMISTIC_WRITE;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
@@ -27,6 +24,15 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             value = "select p " +
                     "from Post p " +
                     "join fetch p.member m " +
+                    "join fetch p.images i " +
+                    "where p.id = :postId"
+    )
+    Optional<Post> findWithMemberAndImagesById(Long postId);
+
+    @Query(
+            value = "select p " +
+                    "from Post p " +
+                    "join fetch p.member m " +
                     "where (:category is null or p.category = :category) and " +
                     "(:cursorId is null or p.id < :cursorId)"
     )
@@ -35,12 +41,4 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             @Param("cursorId") Long cursorId,
             Pageable pageable
     );
-
-    @Lock(PESSIMISTIC_WRITE)
-    @Query(
-            value = "select p " +
-                    "from Post p " +
-                    "where p.id = :postId"
-    )
-    Optional<Post> findByIdWithPessimisticLock(Long postId);
 }
