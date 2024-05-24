2e1bd016ecd8c321238031abe5ea07217d104feb
yurim0628 <dbfla0303@naver.com>
2024-05-24 21:49:01 +0900

    feat: #38 이미지 목록 응답 필드에 추가

--- src/main/java/com/example/sabujak/post/dto/PostResponse.java
+++ src/main/java/com/example/sabujak/post/dto/PostResponse.java
@@ -1,10 +1,13 @@
 package com.example.sabujak.post.dto;
 
+import com.example.sabujak.image.entity.Image;
 import com.example.sabujak.member.entity.Member;
 import com.example.sabujak.post.entity.Category;
 import com.example.sabujak.post.entity.Post;
 import com.example.sabujak.post.entity.Tag;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record PostResponse(
         Long postId,
@@ -16,12 +19,14 @@ public record PostResponse(
         int viewCount,
         int likeCount,
         int commentCount,
+        List<String> images,
         WriterResponse writer,
         boolean isWriter,
         boolean isLiked
 ) {
     public static PostResponse of(Post post, Member member, boolean isWriter, boolean isLiked) {
-        WriterResponse writer = WriterResponse.of(member);
+        List<String> images = post.getImages().stream().map(Image::getImageUrl).toList();
+        WriterResponse writer = com.example.sabujak.post.dto.WriterResponse.of(member);
         return new PostResponse(
                 post.getId(),
                 post.getCategory(),
@@ -32,6 +37,7 @@ public record PostResponse(
                 post.getViewCount(),
                 post.getLikeCount(),
                 post.getCommentCount(),
+                images,
                 writer,
                 isWriter,
                 isLiked
