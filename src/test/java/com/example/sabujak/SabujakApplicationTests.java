c3f9ced1c2957c1d59555e0cd5b1b299848b1c67
yurim0628 <dbfla0303@naver.com>
2024-05-25 13:13:31 +0900

    Merge remote-tracking branch 'origin/develop' into develop

--- src/test/java/com/example/sabujak/SabujakApplicationTests.java
+++ src/test/java/com/example/sabujak/SabujakApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.sabujak;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SabujakApplicationTests.class)
+@SpringBootTest
 class SabujakApplicationTests {
 
 	@Test
