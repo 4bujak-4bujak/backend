e373de48783fa88cb28195f4ad993e8fad5ef76f
yurim0628 <dbfla0303@naver.com>
2024-05-27 00:52:57 +0900

    feat: #47 findWithOptimisticLockById 작성

--- src/main/java/com/example/sabujak/post/repository/PostRepository.java
+++ src/main/java/com/example/sabujak/post/repository/PostRepository.java
@@ -4,13 +4,19 @@ import com.example.sabujak.post.entity.Category;
 import com.example.sabujak.post.entity.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
+import static jakarta.persistence.LockModeType.*;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Lock(value = OPTIMISTIC)
+    Optional<Post> findWithOptimisticLockById(Long postId);
 
     @Query(
             value = "select p " +
