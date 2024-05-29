39ff397637fd7415d52692384effb8be9d3ca289
KimSangwoo1259 <rlatkddn2902@gmail.com>
2024-05-29 17:33:47 +0900

    feat: #53 새로생긴 image repository 추가

---
+++ src/main/java/com/example/sabujak/branch/repository/BranchImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.sabujak.branch.repository;
+
+import com.example.sabujak.branch.entity.BranchImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchImageRepository extends JpaRepository<BranchImage, Long> {
+}
