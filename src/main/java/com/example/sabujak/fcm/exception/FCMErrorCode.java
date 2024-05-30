b6cd9020c589f6e1972be8c4d5c732a882105615
yurim0628 <dbfla0303@naver.com>
2024-05-30 18:11:09 +0900

    feat: #52 FCM ErrorCode 추가

--- src/main/java/com/example/sabujak/fcm/exception/FCMErrorCode.java
+++ src/main/java/com/example/sabujak/fcm/exception/FCMErrorCode.java
@@ -12,8 +12,9 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @RequiredArgsConstructor
 public enum FCMErrorCode implements ErrorCode {
 
-    FCM_TOKEN_NOT_FOUND(BAD_REQUEST, "6-001", "FCM Token이 존재하지 않습니다."),
-    FCM_SENDING_MESSAGE_FAILED(INTERNAL_SERVER_ERROR, "6-002", "FCM 알림 전송에 실패했습니다.");
+    FCM_TOKEN_NOT_FOUND(BAD_REQUEST, "6-001", "FCM 토큰이 존재하지 않습니다."),
+    FCM_SERVER_ERROR(INTERNAL_SERVER_ERROR, "6-002", "FCM 서버의 오류로 메시지 전송에 실패했습니다."),
+    FCM_UNAUTHORIZED(INTERNAL_SERVER_ERROR, "6-003", "FCM 인증 오류로 메시지 전송에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String customCode;
