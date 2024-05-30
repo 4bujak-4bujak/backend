952db09377af44f2ca83aa37efb420ec7be086c3
yurim0628 <dbfla0303@naver.com>
2024-05-30 18:28:50 +0900

    feat: #52 FirebaseMessagingException 예외 핸들링

--- src/main/java/com/example/sabujak/fcm/service/FCMNotificationService.java
+++ src/main/java/com/example/sabujak/fcm/service/FCMNotificationService.java
@@ -1,67 +1,75 @@
 package com.example.sabujak.fcm.service;
 
 import com.example.sabujak.fcm.exception.FCMException;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 import static com.example.sabujak.fcm.constants.FCMConstants.*;
-import static com.example.sabujak.fcm.exception.FCMErrorCode.FCM_SENDING_MESSAGE_FAILED;
+import static com.example.sabujak.fcm.exception.FCMErrorCode.*;
+import static com.google.firebase.messaging.MessagingErrorCode.*;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class FCMNotificationService {
 
-    private final FCMTokenService FCMTokenService;
+    private final FCMTokenService fcmTokenService;
     private final FirebaseMessaging firebaseMessaging;
 
-    public void sendFCMNotification(String email, String content, String notificationId, String targetUrl) {
-        log.info("Sending FCM Notification. Receiver Email: [{}]", email);
-        String token = getFCMToken(email);
-        Notification notification = createNotification(DEFAULT_TITLE, content);
-        Map<String, String> data = createData(notificationId, targetUrl);
-        Message message = createMessage(token, notification, data);
-        try {
-            String response = firebaseMessaging.send(message);
-            log.info("FCM Notification Sent Successfully. Message ID: [{}]", response);
-        } catch (FirebaseMessagingException e) {
-            log.error("Failed To Send FCM Notification. Exception Message: [{}]", e.getMessage());
-            throw new FCMException(FCM_SENDING_MESSAGE_FAILED);
-        }
+    public Message createFCMMessage(String email, String content, Long notificationId, String targetUrl) {
+        return Message.builder()
+                .setToken(fcmTokenService.getFCMToken(email))
+                .setNotification(createNotification(content))
+                .putAllData(createData(notificationId, targetUrl))
+                .build();
     }
 
-    private String getFCMToken(String email) {
-        return FCMTokenService.getFCMToken(email);
-    }
-
-    private Notification createNotification(String title, String body) {
-        log.info("Creating Notification. Notification Title: [{}}, Body: [{}]", title, body);
+    private Notification createNotification(String body) {
         return Notification.builder()
-                .setTitle(title)
+                .setTitle(DEFAULT_TITLE)
                 .setBody(body)
                 .build();
     }
 
-    private Map<String, String> createData(String notificationId, String targetUrl) {
-        log.info("Creating Data. Notification ID: [{}], Target URL: [{}]", notificationId, targetUrl);
+    private Map<String, String> createData(Long notificationId, String targetUrl) {
         return Map.of(
-                NOTIFICATION_ID_KEY, notificationId,
+                NOTIFICATION_ID_KEY, notificationId.toString(),
                 TARGET_URL_KEY, targetUrl
         );
     }
 
-    private Message createMessage(String token, Notification notification, Map<String, String> data) {
-        return Message.builder()
-                .setToken(token)
-                .setNotification(notification)
-                .putAllData(data)
-                .build();
+    @Retryable(
+            retryFor = FirebaseMessagingException.class,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
+    public void sendFCMNotification(String email, Message message) throws FirebaseMessagingException {
+        try {
+            String response = firebaseMessaging.send(message);
+            log.info("FCM Notification Sent Successfully. Message ID: [{}]", response);
+        } catch (FirebaseMessagingException e) {
+            MessagingErrorCode errorCode = e.getMessagingErrorCode();
+            if (errorCode.equals(INTERNAL) || errorCode.equals(UNAVAILABLE)) {
+                log.info("Failed To Send FCM Notification. FCM Server Error. Retrying...");
+                throw e;
+            } else if (errorCode.equals(UNREGISTERED) || errorCode.equals(INVALID_ARGUMENT)) {
+                log.info("Failed To Send FCM Notification. Invalid FCM Token. Deleting...");
+                fcmTokenService.removeFCMToken(email);
+                throw new FCMException(INVALID_FCM_TOKEN);
+            } else if (errorCode.equals(THIRD_PARTY_AUTH_ERROR) || errorCode.equals(SENDER_ID_MISMATCH)) {
+                throw new FCMException(FCM_UNAUTHORIZED);
+            }
+        }
+    }
+
+    @Recover
+    private void recover(FirebaseMessagingException e, String email, Message message) {
+        throw new FCMException(FCM_SERVER_ERROR);
     }
 }
