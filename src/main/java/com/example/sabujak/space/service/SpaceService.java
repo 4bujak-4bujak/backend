def33dd08959cc0df7e77583b06b71bd7329ee36
zoomin3022 <zoomin3022@gmail.com>
2024-05-31 20:49:57 +0900

    refactor: #37 실수로 빠진 readOnly 추가

--- src/main/java/com/example/sabujak/space/service/SpaceService.java
+++ src/main/java/com/example/sabujak/space/service/SpaceService.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 @Service
 public class SpaceService {
     private final MeetingRoomRepository meetingRoomRepository;
