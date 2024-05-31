58ae3c49b62a961c4d95b6c8013668aa6e8c1d93
yurim0628 <dbfla0303@naver.com>
2024-05-31 17:26:29 +0900

    refactor: #52 로그 제거

--- src/main/java/com/example/sabujak/fcm/config/FCMConfig.java
+++ src/main/java/com/example/sabujak/fcm/config/FCMConfig.java
@@ -1,11 +1,9 @@
 package com.example.sabujak.fcm.config;
 
-import com.example.sabujak.common.exception.CommonException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +12,8 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static com.example.sabujak.common.exception.CommonErrorCode.COMMON_SYSTEM_ERROR;
 import static com.google.firebase.FirebaseApp.DEFAULT_APP_NAME;
 
-@Slf4j
 @Configuration
 public class FCMConfig {
 
@@ -40,24 +36,22 @@ public class FCMConfig {
     }
 
     private FirebaseApp createFirebaseApp() {
-        try {
-            GoogleCredentials credentials = createGoogleCredentials();
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(credentials)
-                    .build();
-            FirebaseApp app = FirebaseApp.initializeApp(options);
-            log.info("FirebaseApp initialized. App Name: [{}]", app.getName());
-            return app;
-        } catch (IOException e) {
-            throw new CommonException(COMMON_SYSTEM_ERROR);
-        }
+        GoogleCredentials credentials = createGoogleCredentials();
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+        return FirebaseApp.initializeApp(options);
     }
 
-    private GoogleCredentials createGoogleCredentials() throws IOException {
-        ClassPathResource resource = new ClassPathResource(path);
-        InputStream credentialsStream = resource.getInputStream() ;
-        return GoogleCredentials
-                .fromStream(credentialsStream)
-                .createScoped(scope);
+    private GoogleCredentials createGoogleCredentials() {
+        try {
+            ClassPathResource resource = new ClassPathResource(path);
+            InputStream credentialsStream = resource.getInputStream();
+            return GoogleCredentials
+                    .fromStream(credentialsStream)
+                    .createScoped(scope);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
