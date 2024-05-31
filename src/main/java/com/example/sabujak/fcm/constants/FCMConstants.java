06feb30eaa804fb6c477d2815c7b21b4da0f45b0
yurim0628 <dbfla0303@naver.com>
2024-05-31 23:04:33 +0900

    refactor: #52 안 쓰는 상수 필드 제거

--- src/main/java/com/example/sabujak/fcm/constants/FCMConstants.java
+++ src/main/java/com/example/sabujak/fcm/constants/FCMConstants.java
@@ -5,9 +5,6 @@ public class FCMConstants {
     public static final String FCM_TOKEN_PREFIX = "fcm_token:";
     public static final long FCM_TOKEN_EXPIRATION = 1000L * 60 * 60 * 24 * 60;
 
-    public static final String COMMUNITY_NOTIFICATION_URL_PREFIX = "/posts/";
-
-    public static final String DEFAULT_TITLE = "Offispace";
-    public static final String NOTIFICATION_ID_KEY = "notificationId";
+    public static final String DEFAULT_TITLE = "offispace";
     public static final String TARGET_URL_KEY = "targetUrl";
 }
