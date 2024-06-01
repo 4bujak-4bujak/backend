57c1f9999851165604c40845b878dce73ef2ee98
yurim0628 <139152515+yurim0628@users.noreply.github.com>
2024-06-01 18:50:53 +0900

    Update FCMConfig.java
    Signed-off-by: yurim0628 <139152515+yurim0628@users.noreply.github.com>

--- src/main/java/com/example/sabujak/fcm/config/FCMConfig.java
+++ src/main/java/com/example/sabujak/fcm/config/FCMConfig.java
@@ -1,19 +1,25 @@
 package com.example.sabujak.fcm.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static com.google.firebase.FirebaseApp.DEFAULT_APP_NAME;
 
+@Slf4j
 @Configuration
 public class FCMConfig {
 
@@ -47,8 +53,15 @@ public class FCMConfig {
         try {
             ClassPathResource resource = new ClassPathResource(path);
             InputStream credentialsStream = resource.getInputStream();
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNode = mapper.readTree(credentialsStream);
+            String jsonString = jsonNode.toString();
+
+            InputStream validatedCredentialsStream = new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8));
+
             return GoogleCredentials
-                    .fromStream(credentialsStream)
+                    .fromStream(validatedCredentialsStream)
                     .createScoped(scope);
         } catch (IOException e) {
             throw new RuntimeException(e);
