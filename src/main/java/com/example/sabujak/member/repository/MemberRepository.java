8a886e9722d10e323a2da77e559458c28eaf758c
zoomin3022 <zoomin3022@gmail.com>
2024-06-02 18:03:20 +0900

    refactor: @EntityGraph -> @Query
    EntityGraph 시 알 수 없는 쿼리가 나감

--- src/main/java/com/example/sabujak/member/repository/MemberRepository.java
+++ src/main/java/com/example/sabujak/member/repository/MemberRepository.java
@@ -1,16 +1,21 @@
 package com.example.sabujak.member.repository;
 
 import com.example.sabujak.member.entity.Member;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     Optional<Member> findByMemberEmail(String memberEmail);
+
     Boolean existsByMemberEmail(String memberEmail);
+
     Boolean existsByMemberPhone(String memberPhone);
 
-    @EntityGraph(attributePaths = {"company", "image"})
+    @Query("SELECT m FROM Member m JOIN FETCH m.company JOIN FETCH m.image WHERE m.memberEmail = :memberEmail")
     Optional<Member> findWithCompanyAndImageByMemberEmail(String memberEmail);
+
+    List<Member> findByMemberIdIn(List<Long> memberIds);
 }
