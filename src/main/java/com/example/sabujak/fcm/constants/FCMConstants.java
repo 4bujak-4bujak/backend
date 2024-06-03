d1f784ad04a44570a46b53c778bef5e3f81fe75d
yurim0628 <dbfla0303@naver.com>
2024-06-03 19:22:59 +0900

    refactor: 이미지 추가 및 Target URL 수정

--- src/main/java/com/example/sabujak/fcm/constants/FCMConstants.java
+++ src/main/java/com/example/sabujak/fcm/constants/FCMConstants.java
@@ -5,7 +5,7 @@ public class FCMConstants {
     public static final String FCM_TOKEN_PREFIX = "fcm_token:";
     public static final long FCM_TOKEN_EXPIRATION = 1000L * 60 * 60 * 24 * 60;
 
-    public static final String COMMUNITY_URL_PREFIX = "/community";
+    public static final String COMMUNITY_URL_PREFIX = "/community/";
     public static final String DEFAULT_TITLE = "offispace";
     public static final String DEFAULT_IMAGE = "https://sabujak-image-bucket.s3.ap-northeast-2.amazonaws.com/offispace+icon.png";
     public static final String TARGET_URL_KEY = "targetUrl";
