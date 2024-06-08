7375c31572934e23bf8df82011205276d374a3c4
KimSangwoo1259 <rlatkddn2902@gmail.com>
2024-06-08 19:07:37 +0900

    feat: #112 meetingroom type별 개수 반환 로직 추가

--- src/main/java/com/example/sabujak/space/repository/meetingroom/MeetingRoomRepository.java
+++ src/main/java/com/example/sabujak/space/repository/meetingroom/MeetingRoomRepository.java
@@ -1,6 +1,8 @@
 package com.example.sabujak.space.repository.meetingroom;
 
+import com.example.sabujak.branch.entity.Branch;
 import com.example.sabujak.space.entity.MeetingRoom;
+import com.example.sabujak.space.entity.MeetingRoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,6 @@ public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Long>,
 
     @Query("SELECT m FROM MeetingRoom m LEFT JOIN FETCH m.branch WHERE m.spaceId = :meetingRoomId")
     Optional<MeetingRoom> findByMeetingRoomIdWithBranch(Long meetingRoomId);
+
+    Integer countAllByBranchAndMeetingRoomType(Branch branch, MeetingRoomType meetingRoomType);
 }
