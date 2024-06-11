ad04190bfe00e1235b639c934d51d402eccb75f9
KimSangwoo1259 <rlatkddn2902@gmail.com>
2024-06-11 22:43:50 +0900

    feat: #168 controller 에 로직 추가

--- src/main/java/com/example/sabujak/member/controller/MemberController.java
+++ src/main/java/com/example/sabujak/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.sabujak.member.controller;
 
 import com.example.sabujak.common.response.Response;
 import com.example.sabujak.member.dto.request.MemberRequestDto;
+import com.example.sabujak.member.dto.response.MemberModalIgnoredResponseDto;
 import com.example.sabujak.member.dto.response.MemberResponseDto;
 import com.example.sabujak.member.service.MemberService;
 import com.example.sabujak.security.dto.request.AuthRequestDto;
@@ -20,6 +21,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
 
 @Tag(name = "members", description = "회원 API")
 //swagger
@@ -95,6 +98,34 @@ public class MemberController {
     public ResponseEntity<Response<Void>> changeMemberImage(@AuthenticationPrincipal AuthRequestDto.Access access,
                                                             @RequestPart MultipartFile image) {
         memberService.changeMemberImage(access.getEmail(), image);
+        return ResponseEntity.ok(Response.success(null));
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = Response.class))),
+            @ApiResponse(responseCode = "404", description = "조회 실패", content = @Content(schema = @Schema(implementation = Response.class)))})
+    @Operation(summary = "회원 모달 다시보지 않기 체크 여부 조회", description = "회원이 24시간 내에 다시보지않기를 체크했는지 여부를 조회한다.")
+    @Parameters({
+            @Parameter(name = "access", hidden = true)
+    })
+    @GetMapping("/members/modal")
+    public ResponseEntity<Response<MemberModalIgnoredResponseDto>> checkMemberModalIgnored( @AuthenticationPrincipal AuthRequestDto.Access access
+    )
+    {
+        return ResponseEntity.ok(Response.success(memberService.checkModalIgnored(access.getEmail())));
+    }
+
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "업데이트 성공", content = @Content(schema = @Schema(implementation = Response.class))),
+            @ApiResponse(responseCode = "404", description = "업데이트 실패", content = @Content(schema = @Schema(implementation = Response.class)))})
+    @Operation(summary = "회원 모달 다시보지 않기 체크 여부 업데이트", description = "회원이 24시간 내에 다시보지않기를 체크했는지 여부를 업데이트. ignored = false -> 다시보지않기 누른 적 없거나 24시간이 지남 따라서 모달 띄워야함")
+    @Parameters({
+            @Parameter(name = "access", hidden = true)
+    })
+    @PatchMapping("/members/modal")
+    public ResponseEntity<Response<Void>> changeMemberModalIgnored( @AuthenticationPrincipal AuthRequestDto.Access access){
+        memberService.changeModalIgnored(access.getEmail());
         return ResponseEntity.ok(Response.success(null));
     }
 
